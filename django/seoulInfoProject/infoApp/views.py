3c0b931bc1768af30bf38867112363bc2c7a6615
TJ Park <hahxowns@naver.com>
2023-08-16 21:06:12 +0900

    Update views.py

--- django/seoulInfoProject/infoApp/views.py
+++ django/seoulInfoProject/infoApp/views.py
@@ -13,7 +13,10 @@ def placeList(request):
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
-    context = {"page_obj": page_obj}
+    context = {
+        "page_obj": page_obj
+    
+    }
     return render(request, PATH + "infoPages/placeList.html", context)
 
 
