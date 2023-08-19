4b31d130887ed2842dfb74eefaa1bd8c200e628c
usiohc <89768234+usiohc@users.noreply.github.com>
2023-08-19 18:07:58 +0900

    add: Develop airflow test  (#12)
    * add: airflow CD
    
    * add: airflow CI
    
    * Fix code style issues with Black
    
    * fix: airflow CI path
    
    * fix: airflow CI var-file
    
    * fix: requirements-file path
    
    * fix: restore path
    
    ---------
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- django/seoulInfoProject/seoulInfoProject/asgi.py
+++ django/seoulInfoProject/seoulInfoProject/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seoulInfoProject.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seoulInfoProject.settings")
 
 application = get_asgi_application()
