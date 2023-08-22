aac4c471afda78a9cb31d69a310bcd4836bd0b92
Lint Action <lint-action@samuelmeuli.com>
2023-08-22 04:03:48 +0000

    Fix code style issues with Black

--- django/seoulInfoProject/info_app/apps.py
+++ django/seoulInfoProject/info_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class InfoAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'info_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "info_app"
--- django/seoulInfoProject/info_app/models.py
+++ django/seoulInfoProject/info_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Place(models.Model):
     place_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
