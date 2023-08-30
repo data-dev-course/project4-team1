387e7cd0e2f018a11b8ffc1927a073de531d26d1
usiohc <asltn99@gmail.com>
2023-08-30 12:15:00 +0900

    add: cron_news_data

---
+++ django/seoulInfoProject/cron/cron.py
@@ -0,0 +1,70 @@
+from django.conf import settings
+from .env import *
+import os
+
+# from pytz import timezone
+from datetime import datetime
+
+# KST = timezone("Asia/Seoul")
+
+
+def download_data_news():
+    import boto3
+
+    print("=" * 10 + "download_data_news" + "=" * 10)
+    AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+    AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+    AWS_S3_REGION_NAME = "ap-northeast-2"
+    DATA_DIR = getattr(settings, "DATA_DIR", "")
+    # today = datetime.now(KST).strftime("%Y%m%d")
+    today = datetime.now().strftime("%Y%m%d")
+
+    session = boto3.Session(
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=AWS_S3_REGION_NAME,
+    )
+
+    s3 = session.resource("s3")
+    bucket = s3.Bucket(name="de-3-1")
+
+    area_list_news = AREA_LIST_NEWS
+    for area in area_list_news:
+        try:
+            file_path = DATA_DIR / f"news/{area}"
+            mkdir(file_path)
+
+            file_name = f"{area}-{today}.json"  # 파일 이름
+            bucket_path = f"area_news/{area}"  # 버켓 주소
+
+            bucket.download_file(f"{bucket_path}/{file_name}", f"{file_path}/{file_name}")
+        except Exception as e:
+            print(f"실패 장소: {area} | error message: {e}")
+
+    print("=" * 30)
+
+
+def mkdir(dir):
+    try:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+    except OSError:
+        print("Error: Failed to create the directory.")
+
+
+def rm_news():
+    import subprocess
+
+    print("=" * 10 + "rm_news" + "=" * 10)
+    rm = subprocess.run(["sh", "cron/rm_news.sh"], capture_output=True, encoding="utf-8")
+    if rm.stdout:
+        print(rm.stdout)
+    else:
+        print("2일 이전의 json 파일이 없습니다.")
+    print("=" * 25)
+    # sudo find /var/app/current/data/news -name '*.json' -mtime +2 -delete
+
+
+if __name__ == "__main__":
+    download_data_news()
+    # rm_news()
