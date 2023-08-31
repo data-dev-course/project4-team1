1f0ba7f9c9a16649cabeb545be6d778b34634f38
usiohc <asltn99@gmail.com>
2023-08-31 14:12:24 +0900

    add: 썸네일 이미지 추가

--- django/seoulInfoProject/info_app/view/news.py
+++ django/seoulInfoProject/info_app/view/news.py
@@ -5,7 +5,7 @@ import os
 import json
 
 
-def news_data(area, data_path):
+def news_data(area, data_path) -> json:
     area = area_replace(area)
 
     time = timezone.now()
@@ -19,6 +19,16 @@ def news_data(area, data_path):
     with open(json_path, "r") as f:
         data = json.load(f)
 
+    FLAG = False
+    for i in range(data["display"]):
+        if data["items"][i].get("img") == None:
+            data["items"][i]["img"] = get_meta_og_image(data["items"][i]["link"])
+            FLAG = True
+
+    if FLAG:
+        with open(json_path, "w") as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
+
     return data
 
 
@@ -26,3 +36,20 @@ def area_replace(area) -> str:
     area = str(area)
     new_area = area.replace("·", " ")
     return new_area
+
+
+def get_meta_og_image(url):
+    from bs4 import BeautifulSoup
+    import requests
+
+    try:
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, "html.parser")
+        meta_og_image = soup.find("meta", property="og:image")
+        if meta_og_image:
+            return meta_og_image["content"]
+        else:
+            return "not found -> meta tag og:image "
+    except Exception as e:
+        print(f"get_meta_og_image | error: {e}")
+        return None
