e4c437da35383c3bb3d8958b0dc07bbbd46ae6b4
Lint Action <lint-action@samuelmeuli.com>
2023-08-31 04:04:23 +0000

    Fix code style issues with Black

--- airflow/Dags/seoul_api_data.py
+++ airflow/Dags/seoul_api_data.py
@@ -176,7 +176,6 @@ async def weather_newslist_data(data, area_nm, current_date):
     df1 = pd.DataFrame([extracted_data])
 
     try:
-
         data2 = data1.get("WEATHER_STTS", {}).get("WEATHER_STTS", {}).get("NEWS_LIST")
         if data2:
             if data2 == "None":
@@ -185,8 +184,15 @@ async def weather_newslist_data(data, area_nm, current_date):
                 data2 = [data2]
             df2 = pd.DataFrame(data2)
         else:
-            columns = ['WARN_VAL', 'WARN_STRESS', 'ANNOUNCE_TIME', 'COMMAND', 'CANCEL_YN', 'WARN_MSG']
-            none_data = {col: ['정보없음'] for col in columns}
+            columns = [
+                "WARN_VAL",
+                "WARN_STRESS",
+                "ANNOUNCE_TIME",
+                "COMMAND",
+                "CANCEL_YN",
+                "WARN_MSG",
+            ]
+            none_data = {col: ["정보없음"] for col in columns}
             df2 = pd.DataFrame(none_data)
 
     except KeyError:
